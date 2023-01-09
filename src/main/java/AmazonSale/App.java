1b02a1d999c26dfb9e9a14dc93ea5db322d0e350
kiran <paritalakomali@gmail.com>
2023-01-09 15:10:45 +0530

    first commit

---
+++ src/main/java/AmazonSale/App.java
@@ -0,0 +1,13 @@
+package AmazonSale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
